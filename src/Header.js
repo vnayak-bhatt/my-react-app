8c55e33ca9b31a88779d067ee0ea999960f9705b
vnayak-bhatt <vinayak.bhatt@habilelabs.io>
2019-10-21 19:34:03 +0530

    header.js updated with hook

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-function Header(props){
+export default function Header(props){
+    const [name, setName] = useState('Vinayak');
+    const [surname, setSurname] = useState('Bhatt');
+    const [width, setWidth] = useState(window.innerWidth);
+
+    function handleNameChange(e){
+        setName(e.target.value)
+    }
+
+    function handleSurnameChange(e){
+        setSurname(e.target.value)
+    }
+
+    useEffect(()=>{
+        document.title = name + ' ' + surname;
+    });
+    // useEffect(()=>{
+    //     const handleResize = () => {}
+    // });
+
     return (
-        <React.Fragment>
-            <h1 style={{width: '100%'}}>{props.name}</h1>
-            <p>{props.desc}</p>
-        </React.Fragment>
+        <section>
+            <p>Edit input to change the title of page</p>
+            <input value={name} onChange={handleNameChange}/>
+            <input value={surname} onChange={handleSurnameChange}/>
+            <p>width = {width}</p>
+        </section>
     )
 }
 
-export default Header;
